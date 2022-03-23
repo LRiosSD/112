10b270cdfc48cce85b0b9d610e13083ae098282c
Leonardo Rios <leo78rios@gmail.com>
2022-03-22 23:38:19 -0700

    first commit

---
+++ pages/tests.py
@@ -0,0 +1,42 @@
+from urllib import response
+from django.test import SimpleTestCase
+from django.urls  import reverse
+
+# Create your tests here.
+
+
+
+class SimpleTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response=self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response =  self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_returns_correct_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "index.html")
+        self.assertTemplateUsed(response, "base.html")
+    
+    def test_about_page_returns_correct_templates(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, "about.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_home_page_returns_correct_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Hello, World!")
+
+    def test_about_page_returns_correct_content(self):
+        response = self.client.get("/about/")
+        self.assertContains(response, "About the Author")
+    
+    def test_home_url_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
+
+    def test_about_url_reverse_lookup(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
